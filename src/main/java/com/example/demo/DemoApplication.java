924ee1327c85c26905b633ce515e42f7244db014
root <3100672662@qq.com>
2025-07-24 16:02:54 +0800

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-
+            //what can i do
        // ApplicationContext cx= new ClassPathXmlApplicationContext("");
         SpringApplication.run(DemoApplication.class, args);
     }
