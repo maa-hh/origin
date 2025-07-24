58ff9d770f2de6c8de7c3e00d3055fc4bc8c7ef9
root <3100672662@qq.com>
2025-07-24 16:02:54 +0800

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 @SpringBootApplication
 public class DemoApplication {
 
